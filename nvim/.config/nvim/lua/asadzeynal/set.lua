8244a41ad05ce64e8fbf9b7447ef5dead26879ba
Asad Zeynalov <asadzeynal9@gmail.com>
2024-12-27 18:02:56 +0400

    minor visual tweaks

--- nvim/.config/nvim/lua/asadzeynal/set.lua
+++ nvim/.config/nvim/lua/asadzeynal/set.lua
@@ -1,6 +1,13 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+
+
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
@@ -10,3 +17,4 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 
 vim.g.mapleader = " "
+
