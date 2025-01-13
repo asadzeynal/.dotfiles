70696ee6a9252bd30a707a90f5a28f39a066ae6e
Asad Zeynalov <asadzeynal9@gmail.com>
2025-01-13 16:20:54 +0400

    Add copy to clipboard remap

--- nvim/.config/nvim/lua/asadzeynal/remap.lua
+++ nvim/.config/nvim/lua/asadzeynal/remap.lua
@@ -9,6 +9,9 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>n", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<leader>p", "<cmd>cprev<CR>zz")
 
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
