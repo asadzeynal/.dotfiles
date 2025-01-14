c6ca11dc4cf639e31c6102dcde737acebdd605ed
Asad Zeynalov <asadzeynal9@gmail.com>
2025-01-14 14:34:48 +0400

    add diffget remaps to fugitive

--- nvim/.config/nvim/lua/asadzeynal/lazy/fugitive.lua
+++ nvim/.config/nvim/lua/asadzeynal/lazy/fugitive.lua
@@ -2,6 +2,8 @@ return {
 	"tpope/vim-fugitive",
 	branch = "master",
 	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-	end
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+	end,
 }
