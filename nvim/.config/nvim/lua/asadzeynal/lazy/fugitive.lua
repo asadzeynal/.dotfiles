87a5918f7f8debdf88c70b2075dc633d5fc901f5
Asad Zeynalov <asadzeynal9@gmail.com>
2025-01-14 14:44:27 +0400

    change mappings for fudgitive

--- nvim/.config/nvim/lua/asadzeynal/lazy/fugitive.lua
+++ nvim/.config/nvim/lua/asadzeynal/lazy/fugitive.lua
@@ -3,7 +3,7 @@ return {
 	branch = "master",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+		vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>")
+		vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>")
 	end,
 }
