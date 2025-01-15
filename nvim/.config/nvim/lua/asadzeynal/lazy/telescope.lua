3a360d8cbf2c9582e9824123c4378758f8af67cd
Asad Zeynalov <asadzeynal9@gmail.com>
2025-01-15 18:33:11 +0400

    add visual selection grep to telescope

--- nvim/.config/nvim/lua/asadzeynal/lazy/telescope.lua
+++ nvim/.config/nvim/lua/asadzeynal/lazy/telescope.lua
@@ -10,6 +10,7 @@ return {
 		local telescope = require("telescope")
 		local lga_actions = require("telescope-live-grep-args.actions")
 		local builtin = require("telescope.builtin")
+		local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
 
 		telescope.setup({
 			extensions = {
@@ -27,9 +28,10 @@ return {
 
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		-- vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>sg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-
+		vim.keymap.set({ "x", "v" }, "<leader>sf", function()
+			live_grep_args_shortcuts.grep_visual_selection({ quote = false })
+		end)
 		telescope.load_extension("live_grep_args")
 	end,
 }
