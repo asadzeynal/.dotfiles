3a4833dc76240e55f805a4d4850d36d8bd221c06
Asad Zeynalov <asad.zeynalov@dif.tech>
2025-09-16 18:37:01 +0400

    changed color scheme

--- nvim/.config/nvim/lua/asadzeynal/lazy/ui.lua
+++ nvim/.config/nvim/lua/asadzeynal/lazy/ui.lua
@@ -1,26 +1,8 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		config = function()
-			vim.cmd("colorscheme kanagawa-wave")
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = { theme = "auto" },
-				sections = {
-					lualine_c = {
-						{
-							"filename",
-							show_filename_only = false,
-							path = 1,
-						},
-					},
-				},
-			})
-		end,
-	},
+	"AlexvZyl/nordic.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("nordic").load()
+	end,
 }
