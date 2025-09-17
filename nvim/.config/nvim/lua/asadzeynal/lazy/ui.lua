aea35dbbad678abf6fd80cc0781bc2168759094f
Asad Zeynalov <asad.zeynalov@dif.tech>
2025-09-17 19:51:10 +0400

    return lualine

--- nvim/.config/nvim/lua/asadzeynal/lazy/ui.lua
+++ nvim/.config/nvim/lua/asadzeynal/lazy/ui.lua
@@ -1,8 +1,28 @@
 return {
-	"AlexvZyl/nordic.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("nordic").load()
-	end,
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = { theme = "auto" },
+				sections = {
+					lualine_c = {
+						{
+							"filename",
+							show_filename_only = false,
+							path = 1,
+						},
+					},
+				},
+			})
+		end,
+	},
 }
